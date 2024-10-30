a7d47013e9eaf1115366e71193985dc8ec5a736f
MohammedFalih <yunusfalih123@gmail.com>
2024-10-30 22:34:35 +0530

    Save progress before updating Angular

---
+++ angular/src/app/NgZorroAntdModule.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NzI18nModule } from 'ng-zorro-antd/i18n';
+
+@NgModule({
+  exports: [
+    NzI18nModule
+  ],
+})
+export class NgZorroAntdModule {}
