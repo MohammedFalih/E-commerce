bad05f167890f154207bc83d862864cbe83ef012
MohammedFalih <yunusfalih123@gmail.com>
2024-10-31 09:45:41 +0530

    new componentes register and navbar

--- angular/src/app/NgZorroAntdModule.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NzI18nModule } from 'ng-zorro-antd/i18n';
-
-@NgModule({
-  exports: [
-    NzI18nModule
-  ],
-})
-export class NgZorroAntdModule {}
